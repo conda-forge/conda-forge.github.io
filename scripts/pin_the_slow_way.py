aa4268ac53cad0bcac0042af5c5a61eb0b217216
Amir Mohammadi <183.amir@gmail.com>
2016-09-14 11:06:52 +0200

    Pin giflib on a minor version
    http://abi-laboratory.pro/tracker/timeline/giflib/

--- scripts/pin_the_slow_way.py
+++ scripts/pin_the_slow_way.py
@@ -32,7 +32,7 @@ pinned = {
           'fontconfig': 'fontconfig 2.11.*',
           'freetype': 'freetype 2.6.*',
           'geos': 'geos 3.4.*',
-          'giflib': 'giflib 5.1.2|5.1.2.*',
+          'giflib': 'giflib 5.1.*',
           'hdf5': 'hdf5 1.8.17|1.8.17.*',
           'icu': 'icu 56.*',
           'jpeg': 'jpeg 9*',
