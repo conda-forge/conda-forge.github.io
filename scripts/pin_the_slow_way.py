103b69a9a6204a79af49dc27e1891822be055fb6
Guilherme Quentel Melo <gqmelo@gmail.com>
2017-06-12 14:08:02 -0300

    Fix mpfr pinning

--- scripts/pin_the_slow_way.py
+++ scripts/pin_the_slow_way.py
@@ -61,7 +61,7 @@ pinned = {
           'libtiff': 'libtiff 4.0.*',  # 4.0.6
           'libxml2': 'libxml2 2.9.*',  # 2.9.4
           'metis': 'metis 5.1.*',  # NA
-          'mpfr': 'mpfr 3.*',  # 3.1.5
+          'mpfr': 'mpfr 3.1.*',  # 3.1.5
           'ncurses': 'ncurses 5.9*',  # 5.9
           'netcdf-cxx4': 'netcdf-cxx4 4.3.*',  # NA
           'netcdf-fortran': 'netcdf-fortran 4.4.*',  #
