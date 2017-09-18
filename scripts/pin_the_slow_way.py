b6c9ad4d0db13eabc33485232321502468fc477f
Dougal J. Sutherland <dougal@gmail.com>
2017-09-18 03:36:03 +0100

    pin arpack

--- scripts/pin_the_slow_way.py
+++ scripts/pin_the_slow_way.py
@@ -37,6 +37,7 @@ import conda_smithy.feedstocks as feedstocks
 # As showed by https://abi-laboratory.pro/tracker/timeline/gmp/ binaries built with 6.1.0 are not
 # compatible with 5.0.1 (the other way around would be true).
 pinned = {
+          'arpack': 'arpack 3.5.*',  # NA
           'boost': 'boost 1.65.1',  # 1.61.0
           'boost-cpp': 'boost-cpp 1.65.1',  # NA
           'bzip2': 'bzip2 1.0.*',  # 1.0.6
