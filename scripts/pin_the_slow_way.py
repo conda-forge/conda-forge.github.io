2c9b438ae8d4312b351c92a0e26a5f0f0023ffe0
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-12-18 01:32:04 -0500

    Update the `sqlite` and `tk` pinnings
    These need to be updated to match `defaults`.

--- scripts/pin_the_slow_way.py
+++ scripts/pin_the_slow_way.py
@@ -94,9 +94,9 @@ pinned = {
           'readline': 'readline 7.0',  # 7.0
           'snappy': 'snappy 1.1.6|1.1.7',  # 1.1.6
           'sox': 'sox 14.4.2',  # NA
-          'sqlite': 'sqlite 3.13.*',  # 3.13.0
+          'sqlite': 'sqlite 3.20.*',  # 3.20.1
           'sundials': 'sundials 2.7.*',  # NA
-          'tk': 'tk 8.5.*',  # 8.5.18
+          'tk': 'tk 8.6.*',  # 8.6.7
           'vlfeat': 'vlfeat 0.9.20',  # NA
           'xerces-c': 'xerces-c 3.2.0'  # 3.2.0
           'xz': 'xz 5.2.*',  # 5.2.2
