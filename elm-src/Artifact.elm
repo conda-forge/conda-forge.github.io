ed55e312b431c107cd95ba437907b2f97fb1f7af
Anthony Scopatz <scopatz@gmail.com>
2019-02-07 14:31:18 -0500

    some sep

--- elm-src/Artifact.elm
+++ elm-src/Artifact.elm
@@ -12,7 +12,7 @@ import Json.Decode
 import Json.Decode as Decode
 import Json.Encode as Encode
 import Url
-import Url.Parser exposing (Parser, parse, query, (</>), (<?>), s)
+import Url.Parser exposing (Parser, parse, query, (</>), (<?>), s, oneOf, map)
 import Url.Parser.Query exposing (map4, string)
 import Url.Parser.Query as Query
 
@@ -25,9 +25,19 @@ type alias Model =
     { error : Maybe Http.Error
     , url : Url.Url
     , key : Nav.Key
-    , query : UrlQuery
+    , route : Route
     , response : Maybe Artifact
     }
+
+
+toUrl : String -> Url.Url -> Url.Url
+toUrl s url =
+    case Url.fromString s of
+        Nothing ->
+            url
+        Just u ->
+            u
+            --Maybe.withDefault url u
 
 
 initialModel : Url.Url -> Nav.Key -> Model
@@ -42,6 +52,16 @@ initialModel url key =
         --(artifactFromUrl (parseUrlQuery url))
 
 
+type Route
+    = NotFound
+    | ParsedRoute UrlQuery
+
+route : Parser (Route -> a) a
+route =
+  oneOf
+    [ map ParsedRoute queryParserUrlQuery
+    ]
+
 type alias UrlQuery =
     { pkg : Maybe String
     , channel : Maybe String
@@ -51,18 +71,21 @@ type alias UrlQuery =
 
 
 
-maybeParseUrlQuery : Url.Url -> Maybe UrlQuery
-maybeParseUrlQuery url =
-    parse (query (map4 UrlQuery
+queryParserUrlQuery =
+    query (map4 UrlQuery
         (string "pkg")
         (string "channel")
         (string "arch")
         (string "name")
-    )) url
+    )
 
-parseUrlQuery : Url.Url -> UrlQuery
+
+parseUrlQuery : Url.Url -> Route
 parseUrlQuery url =
-    Maybe.withDefault (UrlQuery (Just "") (Just "") (Just "") (Just "")) (maybeParseUrlQuery url)
+    Maybe.withDefault
+        --(UrlQuery (Just "") (Just "") (Just "") (Just ""))
+        NotFound
+        (parse route url)
 
 type Msg
     = NoOp
@@ -90,7 +113,7 @@ update msg model =
                     ( model, Nav.load href )
 
         UrlChanged url ->
-            ( { model | url = url, query = (parseUrlQuery url) }, Cmd.none )
+            ( { model | url = url, route = (parseUrlQuery url) }, Cmd.none )
 
         Response (Ok response) ->
             ( { model | error = Nothing, response = Just response }, Cmd.none )
@@ -122,15 +145,20 @@ artifactFromUrl urlquery =
 
 -- VIEWS
 
-viewUrlQuery : UrlQuery -> Html Msg
-viewUrlQuery urlquery =
-    div [ class "url-query-text" ]
-        [ h4 [] [ text (
-            (Maybe.withDefault "<no-pkg>" urlquery.pkg) ++ "/" ++
-            (Maybe.withDefault "<no-channel" urlquery.channel) ++ "/" ++
-            (Maybe.withDefault "<no-arch>" urlquery.arch) ++ "/" ++
-            (Maybe.withDefault "<no-name" urlquery.name) )]
-        ]
+viewUrlQuery : Route -> Html Msg
+viewUrlQuery r =
+    case r of
+        NotFound ->
+            div [ class "url-query-text-error" ]
+                [h4 [] [text "URL query not parsed!"]]
+        ParsedRoute urlquery ->
+            div [ class "url-query-text" ]
+            [ h4 [] [ text (
+                (Maybe.withDefault "<no-pkg>" urlquery.pkg) ++ "/" ++
+                (Maybe.withDefault "<no-channel" urlquery.channel) ++ "/" ++
+                (Maybe.withDefault "<no-arch>" urlquery.arch) ++ "/" ++
+                (Maybe.withDefault "<no-name" urlquery.name) )]
+            ]
 
 viewArtifact : Artifact -> Html Msg
 viewArtifact artifact =
@@ -162,13 +190,13 @@ viewError error =
 
 
 viewBody :
-    { a | error : Maybe Http.Error, response : Maybe Artifact, query : UrlQuery, url : Url.Url}
+    { a | error : Maybe Http.Error, response : Maybe Artifact, route : Route, url : Url.Url}
     -> Html Msg
 viewBody model =
     div []
         [ viewHeader
         , text (Url.toString model.url)
-        , viewUrlQuery model.query
+        , viewUrlQuery model.route
         , case model.response of
             Just response ->
                 viewArtifact response
