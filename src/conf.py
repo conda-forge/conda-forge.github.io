68810e8356ca47a46aac821a44ce012ab359cc25
Christian Roth <christian.roth@mpibpc.mpg.de>
2019-02-27 17:16:23 +0100

    fixed navbar

--- src/conf.py
+++ src/conf.py
@@ -31,7 +31,8 @@ import cloud_sptheme as csp
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.todo'
+    'sphinx.ext.todo',
+    'sphinxcontrib.fulltoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
