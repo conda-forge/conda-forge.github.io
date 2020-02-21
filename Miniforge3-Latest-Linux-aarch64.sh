b7040a9b10e0797cc690f3bad6b9564e37e34045
Anthony Scopatz <scopatz@gmail.com>
2020-02-21 15:41:11 -0500

    attempt to see if redirects work on gh-pages

---
+++ Miniforge3-Latest-Linux-aarch64.sh
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="refresh" content="0;url=https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh" />
+        <link rel="canonical" href="https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh" />
+    </head>
+    <body>
+        <h1>
+            The page been moved to <a href="https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh">https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh</a>
+        </h1>
+    </body>
+</html>
