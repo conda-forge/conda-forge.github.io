1bb266df2d78ea9e603fc7885ada4c52057b1672
Travis-CI on github.com/conda-forge/conda-forge.github.io <conda.forge.coordinator@gmail.com>
2020-08-06 14:16:48 +0000

    Re-ran make.py. [ci skip]

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2020.08.05',
+    VERSION: '2020.08.06',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
