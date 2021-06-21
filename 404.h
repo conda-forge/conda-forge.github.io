683bfbafeaed4e0d38adf52cfe5ddbf60db49d93
Michael Glenn <mbg198618@gmail.com>
2021-06-21 12:46:52 -0500

    Create 404.h

---
+++ 404.h
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title> Page not found :( | conda-forge</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="/css/octicons.3.4.1/octicons.css">
+
+    <!-- Custom CSS -->
+    <link href="/css/theme.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container semi-black-alpha" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    <i><img src="/img/anvil_white.png" alt="An anvil" style="height: 1em;"></i>  <span class="light">conda-forge</span>
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a class="page-scroll" href="https://conda-forge.org/#about">About</a>
+                    </li>
+                    <!-- Uncomment to restore search
+                    <li>
+                        <a href="search.html">Search</a>
+                    </li>
+                    -->
+                    <li>
+                        <a href="https://conda-forge.org/docs">Docs</a>
+                    </li>
+                    <li>
+                        <a href="https://conda-forge.org/blog">Blog</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="https://conda-forge.org/#contribute">Contribute</a>
+                    </li>
+                    <li>
+                        <a href="https://conda-forge.org/feedstock-outputs">Packages</a>
+                    </li>
+                    <li>
+                        <a href="https://conda-forge.org/docs/user/announcements.html#announcements">News</a>
+                    </li>
+                    <li>
+                        <a href="https://conda-forge.org/status">Status</a>
+                    </li>
+                    <li>
+                        <a href="docs/orga/getting-in-touch.html">Contact</a>
+                    </li>
+                    <li>
+                        <a href="https://numfocus.salsalabs.org/donate-to-conda-forge/index.html">Donate!</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container semi-black-alpha">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <img src="/img/anvil_white.png" alt="An anvil" style="height: 100px; margin: 40px;">
+                        <h2 class="brand-heading"> conda-forge</h2>
+                        <p class="intro-text">Sorry, the page you were looking for does not exist.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+    <!-- Footer -->
+    <footer>
+        <div class="container text-center">
+            <p>
+            <a href="/index.html">Home</a> |
+            <a href="/index.html#about">About</a> |
+            <!-- <a href="/index.html#contribute">Contribute</a> |
+            <a href="/feedstocks">Feedstocks</a> | -->
+            <a href="https://github.com/conda-forge">conda-forge on GitHub</a>
+            </p>
+            <br/>
+            <p class="small">
+            <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br /><a href="https://conda-forge.org">conda-forge.org</a> is licensed under a <br /> <a href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
+            </p>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/theme.js"></script>
+
+</body>
+
+</html>
