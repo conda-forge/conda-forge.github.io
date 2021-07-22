e22ff37661aedaad2a4dca9b5cee407e4e92c657
conda-forge-coordinator <conda.forge.coordinator@gmail.com>
2021-07-22 19:19:49 +0000

    deploy: e4d587211f187901218424c6411a303f01f9b844

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2021.07.21',
+    VERSION: '2021.07.22',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
