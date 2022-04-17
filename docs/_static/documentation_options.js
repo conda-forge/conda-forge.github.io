38886388a71f68c9f535b461d4a6d5a01257ad15
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-04-17 00:16:52 +0000

    deploy: 22943a5988745c8ebda52353997c14c3138efd76

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2022.04.13',
+    VERSION: '2022.04.17',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
