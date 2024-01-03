1517a6c3439a640a1c699d8b2d59ac93fa439e3c
jaimergp <jaimergp@users.noreply.github.com>
2024-01-03 13:20:43 +0100

    adjust supporter logos (#15)
    * adjust supporter logos
    
    * shuffle order
    
    * fix azure link
    
    * add cirun and quansight to infra
    
    * link whole card
    
    * fix color
    
    * remove randomizer (breaks links in dark/light switches)
    
    * pre-commit

--- src/components/Supporters/index.jsx
+++ src/components/Supporters/index.jsx
@@ -24,6 +24,41 @@ const infrastructure = [
         width: 250,
     },
     {
+        name: "Azure Pipelines",
+        link: "https://azure.microsoft.com/products/devops/",
+        light: "img/supporters/azure.png",
+        dark: "img/supporters/azure.png",
+        width: 250,
+    },
+    {
+        name: "Cirun",
+        link: "https://cirun.io",
+        light: "img/supporters/cirun_light.svg",
+        dark: "img/supporters/cirun_dark.svg",
+        width: 200,
+    },
+    {
+        name: "Digital Ocean",
+        link: "https://www.digitalocean.com/",
+        light: "img/supporters/do_light.svg",
+        dark: "img/supporters/do_dark.svg",
+        width: 250,
+    },
+    {
+        name: "Github",
+        link: "https://github.com",
+        light: "img/supporters/github_light.png",
+        dark: "img/supporters/github_dark.png",
+        width: 150,
+    },
+    {
+        name: "Oracle Cloud",
+        link: "https://www.oracle.com/cloud/",
+        light: "img/supporters/oracle_light.svg",
+        dark: "img/supporters/oracle_dark.svg",
+        width: 180,
+    },
+    {
         name: "Open Source Lab",
         link: "https://osuosl.org/",
         light: "img/supporters/osl_light.svg",
@@ -35,20 +70,20 @@ const infrastructure = [
         link: "https://www.ovhcloud.com/",
         light: "img/supporters/ovh_light.svg",
         dark: "img/supporters/ovh_dark.svg",
-        width: 250,
+        width: 220,
     },
     {
-        name: "Digital Ocean",
-        link: "https://www.digitalocean.com/",
-        light: "img/supporters/do_light.svg",
-        dark: "img/supporters/do_dark.svg",
-        width: 250,
+        name: "Quansight",
+        link: "https://www.quansight.com/",
+        light: "img/supporters/quansight_light.svg",
+        dark: "img/supporters/quansight_dark.svg",
+        width: 80,
     },
     {
-        name: "Oracle Cloud",
-        link: "https://www.oracle.com/cloud/",
-        light: "img/supporters/oracle_light.svg",
-        dark: "img/supporters/oracle_dark.svg",
+        name: "Travis CI",
+        link: "https://www.travis-ci.com/",
+        light: "img/supporters/travis_light.png",
+        dark: "img/supporters/travis_dark.png",
         width: 200,
     },
 ];
@@ -60,6 +95,13 @@ const developer = [
         light: "img/supporters/anaconda_light.svg",
         dark: "img/supporters/anaconda_dark.svg",
         width: 250,
+    },
+    {
+        name: "IOOS Integrated Ocean Observing System",
+        link: "https://ioos.noaa.gov/",
+        light: "img/supporters/ioos.png",
+        dark: "img/supporters/ioos.png",
+        width: 150,
     },
     {
         name: "Nvidia",
@@ -78,8 +120,8 @@ const developer = [
     {
         name: "Quansight Labs",
         link: "https://labs.quansight.org/",
-        light: "img/supporters/quansight_light.svg",
-        dark: "img/supporters/quansight_dark.svg",
+        light: "img/supporters/quansightlabs_light.svg",
+        dark: "img/supporters/quansightlabs_dark.svg",
         width: 250,
     },
     {
@@ -148,19 +190,20 @@ export default function Supporters() {
                     <div className={styles.card}>
                         {financial.map(
                             ({ name, link, light, dark, width }, index) => (
-                                <div className={styles.cardWrapper} key={index}>
-                                    <Link to={link}>
+                                <Link to={link}>
+                                    <div className={styles.cardWrapper} key={index}>
                                         <ThemedImage
                                             className={styles.image}
                                             alt={`${name} logo`}
+                                            title={`Go to ${name}'s website`}
                                             sources={{
                                                 light: useBaseUrl(`${light}`),
                                                 dark: useBaseUrl(`${dark}`),
                                             }}
                                             width={width}
                                         />
-                                    </Link>
-                                </div>
+                                    </div>
+                                </Link>
                             )
                         )}
                     </div>
@@ -177,19 +220,20 @@ export default function Supporters() {
                     <div className={styles.card}>
                         {infrastructure.map(
                             ({ name, link, light, dark, width }, index) => (
-                                <div className={styles.cardWrapper} key={index}>
-                                    <Link to={link}>
+                                <Link to={link}>
+                                    <div className={styles.cardWrapper} key={index}>
                                         <ThemedImage
                                             className={styles.image}
                                             alt={`${name} logo`}
+                                            title={`Go to ${name}'s website`}
                                             sources={{
                                                 light: useBaseUrl(`${light}`),
                                                 dark: useBaseUrl(`${dark}`),
                                             }}
                                             width={width}
                                         />
-                                    </Link>
-                                </div>
+                                    </div>
+                                </Link>
                             )
                         )}
                     </div>
@@ -204,19 +248,20 @@ export default function Supporters() {
                     <div className={styles.card}>
                         {developer.map(
                             ({ name, link, light, dark, width }, index) => (
-                                <div className={styles.cardWrapper} key={index}>
-                                    <Link to={link}>
+                                <Link to={link}>
+                                    <div className={styles.cardWrapper} key={index}>
                                         <ThemedImage
                                             className={styles.image}
                                             alt={`${name} logo`}
+                                            title={`Go to ${name}'s website`}
                                             sources={{
                                                 light: useBaseUrl(`${light}`),
                                                 dark: useBaseUrl(`${dark}`),
                                             }}
                                             width={width}
                                         />
-                                    </Link>
-                                </div>
+                                    </div>
+                                </Link>
                             )
                         )}
                     </div>
