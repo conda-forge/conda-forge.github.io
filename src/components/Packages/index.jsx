f92b3002c85ee3066e825e2cb23ee4bede2a4209
jaimergp <jaimergp@users.noreply.github.com>
2024-01-30 17:53:24 +0100

    Use upstream feedstock-outputs.json

--- src/components/Packages/index.jsx
+++ src/components/Packages/index.jsx
@@ -56,7 +56,7 @@ const Packages = () => {
     const fetchData = async () => {
       try {
         // TODO: Update URL to production JSON payload
-        const response = await fetch("/test-feedstock-outputs.json");
+        const response = await fetch("https://raw.githubusercontent.com/conda-forge/feedstock-outputs/gh-pages/feedstock-outputs.json");
         const data = await response.json();
 
         if (typeof data === "object" && data !== null) {
