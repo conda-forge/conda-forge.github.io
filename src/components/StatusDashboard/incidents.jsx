12dd19819bde241d7b855b370694b514cd51f8cd
Afshin T. Darian <git@darian.link>
2024-03-04 10:15:18 +0000

    Fix incident severity rendering

--- src/components/StatusDashboard/incidents.jsx
+++ src/components/StatusDashboard/incidents.jsx
@@ -11,13 +11,14 @@ const DATE = "YYYY/M/D HH:mm:ss";
 // The GitHub repository with relevant issues.
 const REPO = { owner: "conda-forge", repo: "status" };
 
+// This label indicates danger.
+const MAJOR = "major outage";
+
+// This label indicates warning.
+const DEGRADED = "degraded performance";
+
 // Incident labels we care about.
-const BAD_LABELS = new Set([
-  "investigating",
-  "degraded performance",
-  "major outage",
-  "maintenance"
-]);
+const BAD_LABELS = new Set(["investigating", DEGRADED, MAJOR, "maintenance"]);
 
 // Time period  we care about: 90 days – in milliseconds.
 const PERIOD = 90 * 24 * 60 * 60 * 1000;
@@ -56,6 +57,9 @@ export default function Incidents({ onLoad }) {
       onLoad();
     })();
   }, []);
+  const outage = !!current.size;
+  const severity = outage && current.has(MAJOR) ? "danger" : "warning";
+  const label = severity ? severity === "danger" ? MAJOR : DEGRADED : "";
   return (
     <>
       <div id="incidents" className={styles.toc_anchor}></div>
@@ -64,11 +68,9 @@ export default function Incidents({ onLoad }) {
           <h3>
             Incidents
             {" "}
-            {current.size > 0 && Array.from(current).map((label, index) => (
-              <span className="badge badge--danger"
-                style={{ marginRight: 5 }}
-                key={index}>{label}</span>
-            ))}
+            {current.size && (
+              <span className={`badge badge--${severity}`}>{label}</span>
+            )}
           </h3>
         </div>
         <div className={`card__body ${styles.incidents}`}>
@@ -85,17 +87,13 @@ function Incident({ children }) {
   const open = issue.state === "open";
   const date = moment(open ? issue.updated_at : issue.closed_at);
   const status = open ? "Ongoing" : "Resolved";
+  const severity = open ?
+    issue.severity === MAJOR ? "danger" : "warning" : "success";
   return (
     <div className={styles.incident}>
       <div>
-        <span
-          className={styles.incident_status}
-          style={{
-            backgroundColor: status === 'Ongoing' ?
-              "var(--ifm-color-warning)" :
-              "var(--ifm-color-success)"
-          }}>{status}</span>
-        <em>{date.format(DATE)}</em>
+        <span className={`badge badge--${severity}`}>{status}</span>
+        <em className={styles.incident_date}>{date.format(DATE)}</em>
       </div>
       <Link className={styles.incident_link} to={issue.html_url}>
         {issue.title} (#{issue.number})
