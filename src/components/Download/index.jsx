66374590ad60a9fd728c245f08201a9300987094
Afshin T. Darian <git@darian.link>
2024-03-12 13:32:19 +0000

    User `filter(1)`

--- src/components/Download/index.jsx
+++ src/components/Download/index.jsx
@@ -8,44 +8,44 @@ export default function Download() {
   const downloads = [
       {
           arch: "arm64 (Apple Silicon)",
-          dark: "img/download/apple-dark.png",
+          dark: "img/download/apple.svg",
           href: "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh",
-          light: "img/download/apple-light.png",
+          light: "img/download/apple.svg",
           os: "macOS",
       },
       {
           arch: "x86_64 (Intel)",
-          dark: "img/download/apple-dark.png",
+          dark: "img/download/apple.svg",
           href: "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-x86_64.sh",
-          light: "img/download/apple-light.png",
+          light: "img/download/apple.svg",
           os: "macOS",
       },
       {
           arch: "x86_64 (amd64)",
-          dark: "img/download/linux-dark.png",
+          dark: "img/download/linux.svg",
           href: "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh",
-          light: "img/download/linux-light.png",
+          light: "img/download/linux.svg",
           os: "Linux",
       },
       {
           arch: "aarch64 (arm64)",
-          dark: "img/download/linux-dark.png",
+          dark: "img/download/linux.svg",
           href: "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh",
-          light: "img/download/linux-light.png",
+          light: "img/download/linux.svg",
           os: "Linux",
       },
       {
           arch: "ppc64le (POWER8/9)",
-          dark: "img/download/linux-dark.png",
+          dark: "img/download/linux.svg",
           href: "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-ppc64le.sh",
-          light: "img/download/linux-light.png",
+          light: "img/download/linux.svg",
           os: "Linux",
       },
       {
           arch: "x86_64",
-          dark: "img/download/windows-dark.png",
+          dark: "img/download/windows.svg",
           href: "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Windows-x86_64.exe",
-          light: "img/download/windows-light.png",
+          light: "img/download/windows.svg",
           os: "Windows",
       }
   ];
@@ -55,8 +55,8 @@ export default function Download() {
             <ThemedImage
                 alt="3D-Anvil illustration for conda-forge"
                 sources={{
-                    light: useBaseUrl("/img/anvil-light.svg"),
-                    dark: useBaseUrl("/img/anvil-dark.svg"),
+                  light: useBaseUrl("/img/anvil-light.svg"),
+                  dark: useBaseUrl("/img/anvil-dark.svg"),
                 }}
             />
         </div>
@@ -66,7 +66,7 @@ export default function Download() {
                 <div className={styles.cardWrapper}>
                   <div className={styles.card}>
                     <ThemedImage
-                      className={styles.image}
+                      className={styles.os_image}
                       alt={`${os} logo`}
                       title={`Download miniforge installer for ${os} ${arch}`}
                       sources={{
