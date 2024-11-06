d1659305ae565bf786af71715b8058d9947151f7
jaimergp <jaimergp@users.noreply.github.com>
2024-11-06 22:11:09 +0100

    tune css filters

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -17,7 +17,7 @@ export default function Calendar() {
       src={`https://calendar.google.com/calendar/embed?height=500&wkst=${firstDay}&ctz=${timezone}&showTitle=0&showTz=1&showPrint=0&src=ODgwNTU3MGE0ZTFjYTIzMTk4NDI5NzFkYjQzODBlZDUxOGM0OTA1NzdjMDY0NTRhZGYyMzAzNzM0NTA2ZjM5N0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%2333b679`}
       width="100%"
       height="500"
-      style={ colorMode === "dark" ? {filter: "invert(100%) hue-rotate(180deg)"} : {}}
+      style={ colorMode === "dark" ? {filter: "invert(95%) brightness(95%) hue-rotate(180deg)"} : {}}
     ></iframe>
   );
 }
