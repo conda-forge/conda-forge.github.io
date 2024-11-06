a83614eb55d21e7c810d1de8e16f8cc4e28b18b7
jaimergp <jaimergp@users.noreply.github.com>
2024-11-06 21:54:29 +0100

    fix performance issue and enable dark mode

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,24 +1,23 @@
 import { React, useEffect, useState } from "react";
+import { useColorMode } from '@docusaurus/theme-common';
 
 export default function Calendar() {
-  const [state, setState] = useState({
-    firstDay: 1,
-    timezone: "UTC",
-  });
+  const [firstDay, setFirstDay] = useState(1);
+  const [timezone, setTimezone] = useState("UTC");
   if (Intl) {
     useEffect(() => {
       const locale = Intl.DateTimeFormat().resolvedOptions().locale;
-      const firstDay = (new Intl.Locale(locale)?.weekInfo?.firstDay ?? 0) + 1;
-      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-      setState({ firstDay: firstDay, timezone: timezone });
+      setFirstDay((new Intl.Locale(locale)?.weekInfo?.firstDay ?? 0) + 1);
+      setTimezone(Intl.DateTimeFormat().resolvedOptions().timeZone);
     });
   }
+  const { colorMode } = useColorMode();
   return (
     <iframe
-      src={`https://calendar.google.com/calendar/embed?height=500&wkst=${state.firstDay}&ctz=${state.timezone}&showTitle=0&showTz=1&showPrint=0&src=ODgwNTU3MGE0ZTFjYTIzMTk4NDI5NzFkYjQzODBlZDUxOGM0OTA1NzdjMDY0NTRhZGYyMzAzNzM0NTA2ZjM5N0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%2333b679`}
+      src={`https://calendar.google.com/calendar/embed?height=500&wkst=${firstDay}&ctz=${timezone}&showTitle=0&showTz=1&showPrint=0&src=ODgwNTU3MGE0ZTFjYTIzMTk4NDI5NzFkYjQzODBlZDUxOGM0OTA1NzdjMDY0NTRhZGYyMzAzNzM0NTA2ZjM5N0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%2333b679`}
       width="100%"
       height="500"
-      frameborder="0"
+      style={ colorMode === "dark" ? {filter: "invert(100%) hue-rotate(180deg)"} : {}}
     ></iframe>
   );
 }
