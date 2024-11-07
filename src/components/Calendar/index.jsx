23c17db076e1b18bf2e101753e4b35be6875d2e1
jaimergp <jaimergp@users.noreply.github.com>
2024-11-07 10:45:10 +0100

    Add news about new core meeting times

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -19,15 +19,20 @@ export default function Calendar() {
     setThemeMode(colorMode);
   });
   return (
+    <>
     <iframe
       src={`https://calendar.google.com/calendar/embed?height=500&wkst=${firstDay}&ctz=${timezone}&showTitle=0&showTz=1&showPrint=0&src=ODgwNTU3MGE0ZTFjYTIzMTk4NDI5NzFkYjQzODBlZDUxOGM0OTA1NzdjMDY0NTRhZGYyMzAzNzM0NTA2ZjM5N0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%2333b679`}
       width="100%"
       height="500"
       style={
         themeMode === "dark"
-          ? { filter: "invert(95%) brightness(95%) hue-rotate(180deg)" }
-          : {}
+        ? { filter: "invert(95%) brightness(95%) hue-rotate(180deg)" }
+        : {}
       }
-    ></iframe>
+      ></iframe>
+    <blockquote>
+      If you cannot see a calendar above this message, please check the <a href={`https://calendar.google.com/calendar/embed?height=500&wkst=${firstDay}&ctz=${timezone}&showTitle=0&showTz=1&showPrint=0&src=ODgwNTU3MGE0ZTFjYTIzMTk4NDI5NzFkYjQzODBlZDUxOGM0OTA1NzdjMDY0NTRhZGYyMzAzNzM0NTA2ZjM5N0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%2333b679`} target="_blank" title="Visit the conda community calendar">conda community calendar</a>.
+    </blockquote>
+    </>
   );
 }
