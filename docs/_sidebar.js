851d06ee2dcaabd8cf2e8208d7b5ec16c9237d96
jaimergp <jaimergp@users.noreply.github.com>
2025-10-23 13:03:25 +0200

    Add secret sidebar for Diátaxis structure (#2633)

--- docs/_sidebar.js
+++ docs/_sidebar.js
@@ -3,6 +3,7 @@
 const fs = require("fs");
 const sidebars = {
   docs: JSON.parse(fs.readFileSync(`${__dirname}/_sidebar.json`)),
+  diataxis: JSON.parse(fs.readFileSync(`${__dirname}/_sidebar_diataxis.json`)),
 };
 
 export default sidebars;
