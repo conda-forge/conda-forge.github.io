fcd0604ca76d990266b59e26020f38ab907bed86
jaimergp <jaimergp@users.noreply.github.com>
2026-06-11 09:39:09 +0200

    Unescape rendered errors in <pre>

--- src/components/ErrorMessageDetails/index.jsx
+++ src/components/ErrorMessageDetails/index.jsx
@@ -1,10 +1,17 @@
+function unescapeHTML(html) {
+  console.log(html);
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+  return doc.documentElement.textContent.replace("&#x27;", "'");
+}
+
 export default function ErrorMessageDetails({ details }) {
   console.log(details);
   let children = [];
   if (details.messages) {
     if (details.kind) {
       children.push(<span className="badge badge--warning">
-        {details.kind && details.kind || "error"}
+        {(details.kind && details.kind != "plain") && details.kind || "error"}
         {details.base_branch && ` @ ${details.base_branch}` || null}
         {details.attempts && ` after ${details.attempts.toFixed(1)} attempts` || null}
       </span>)
@@ -20,11 +27,11 @@ export default function ErrorMessageDetails({ details }) {
       )
     }
     for (const message of details.messages) {
-      children.push(<pre>{message}</pre>)
+      children.push(<pre>{unescapeHTML(message)}</pre>)
     }
   /* Legacy: string only values */
   } else {
-    children.push(<pre>{details.toString()}</pre>)
+    children.push(<pre>{unescapeHTML(details.toString())}</pre>)
   }
   return <>{children}</>
 }
