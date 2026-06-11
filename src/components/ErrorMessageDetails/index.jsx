059f4f99904bebe9d0e255d4f94400e30604337e
jaimergp <jaimergp@users.noreply.github.com>
2026-06-11 10:16:44 +0200

    Same line for table badges

--- src/components/ErrorMessageDetails/index.jsx
+++ src/components/ErrorMessageDetails/index.jsx
@@ -9,26 +9,30 @@ export default function ErrorMessageDetails({ details }) {
   console.log(details);
   let children = [];
   if (details.messages) {
-    if (details.kind) {
-      children.push(<span className="badge badge--warning">
+    if (details.kind || details.url) {
+      let header = []
+      if (details.kind) {
+        header.push(<span className="badge badge--warning">
         {(details.kind && details.kind != "plain") && details.kind || "error"}
         {details.base_branch && ` @ ${details.base_branch}` || null}
         {details.attempts && ` after ${details.attempts.toFixed(1)} attempts` || null}
       </span>)
+      }
+      if (details.url) {
+        header.push(
+          <a href={details.url} target="_blank" title="View CI job" style={{ marginLeft: "1em"}}>
+            View CI job{" "}
+            <small className="fa fa-fw">
+              <i className="fa fa-fw fa-arrow-up-right-from-square"></i>
+            </small>
+          </a>
+        )
+      }
+      children.push(<div style={{ display: "inline-flex"}}>{header}</div>)
     }
-    if (details.url) {
-      children.push(
-        <a href={details.url} target="_blank" title="View CI job" style={{ marginLeft: "1em"}}>
-          View CI job{" "}
-          <small className="fa fa-fw">
-            <i className="fa fa-fw fa-arrow-up-right-from-square"></i>
-          </small>
-        </a>
-      )
-    }
-    for (const message of details.messages) {
-      children.push(<pre>{unescapeHTML(message)}</pre>)
-    }
+    details.messages.map((message, index) => (
+      children.push(<pre key={`message-${index}`}>{unescapeHTML(message)}</pre>)
+    ))
   /* Legacy: string only values */
   } else {
     children.push(<pre>{unescapeHTML(details.toString())}</pre>)
